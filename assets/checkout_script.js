f3dde240c1323796a3c713be7657c83663bf98e3
Roia Tembo <roia@pixelolabs.com>
2024-08-20 17:20:57 +0200

    pagespeed

--- assets/checkout_script.js
+++ assets/checkout_script.js
@@ -1,43 +1,49 @@
-$(document).ready(function() {
-  function checkCitrus() {
-    // Configuration
-    var noCitrusStates = ["FL", "TX", "AZ"];
+function deferJS() {
+  $(document).ready(function() {
+    function checkCitrus() {
+      // Configuration
+      var noCitrusStates = ["FL", "TX", "AZ"];
 
-    // End Configuration
-    var stateFound;
-    var foundProhibitedTag = false;
+      // End Configuration
+      var stateFound;
+      var foundProhibitedTag = false;
 
-    stateFound = window.cart.shipping_state;
-    tags = window.cart.tags;
+      stateFound = window.cart.shipping_state;
+      tags = window.cart.tags;
 
-    if (tags.includes('citrus restricted') || tags.includes('citrus-restricted')) {
-      foundProhibitedTag = true;
+      if (tags.includes('citrus restricted') || tags.includes('citrus-restricted')) {
+        foundProhibitedTag = true;
+      }
+
+      if (noCitrusStates.includes(stateFound) && foundProhibitedTag == true) {
+        return true;
+      }
     }
 
-    if (noCitrusStates.includes(stateFound) && foundProhibitedTag == true) {
-      return true;
+    if (checkCitrus() && $('.step').attr('data-step') == 'shipping_method') {
+        console.log('Checkout Error.');
+
+        var citrusNoticeString = 'As mentioned in our <a href="https://www.fourwindsgrowers.com/pages/shipping-information">Shipping Terms</a>, we are unable to ship citrus items to Texas, Arizona, or Florida. Please remove citrus items or change your shipping address.';
+
+        if ($('.notice--error').length) {
+          $('.notice--error').find('.notice__text').html(citrusNoticeString);
+        } else {
+          $('.section__content').html($('#alert__template').contents());
+        }
     }
-  }
 
-  if (checkCitrus() && $('.step').attr('data-step') == 'shipping_method') {
-      console.log('Checkout Error.');
+    function throwWeatherRestriction() {
+      $('.section--shipping-method').before($('#alert__weather').contents());
+    }
 
-      var citrusNoticeString = 'As mentioned in our <a href="https://www.fourwindsgrowers.com/pages/shipping-information">Shipping Terms</a>, we are unable to ship citrus items to Texas, Arizona, or Florida. Please remove citrus items or change your shipping address.';
+    window.cart.weather_states.forEach(function(curParent) {
+        if ( curParent.toLowerCase() == window.cart.shipping_state.toLowerCase()) {
+          throwWeatherRestriction();
+        }
+    })
+  });
+}
 
-      if ($('.notice--error').length) {
-        $('.notice--error').find('.notice__text').html(citrusNoticeString);
-      } else {
-        $('.section__content').html($('#alert__template').contents());
-      }
-  }
-
-  function throwWeatherRestriction() {
-    $('.section--shipping-method').before($('#alert__weather').contents());
-  }
-
-  window.cart.weather_states.forEach(function(curParent) {
-      if ( curParent.toLowerCase() == window.cart.shipping_state.toLowerCase()) {
-        throwWeatherRestriction();
-      }
-  })
-});
+window.onload = function() {
+  deferJS();
+};
